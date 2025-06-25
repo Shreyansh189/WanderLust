325e0e28045200c0a301185f9f5763544a93797e
SHREYANSH GHANEKAR <shreyansh121221@gmail.com>
2025-06-25 16:17:33 +0530

    second commit

--- models/listing.js
+++ models/listing.js
@@ -3,9 +3,9 @@ const Schema= mongoose.Schema;
 const  listingSchema=new Schema({
     title: { type: String, required: true },
     description: { type: String, required: true },
-    Image: { type: String, 
+  image: { type: String, 
         default:"https://plus.unsplash.com/premium_photo-1684508638760-72ad80c0055f?q=80&w=1171&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-        set:(v)=>v===""? 'default':v,
+        set:(v)=>v===""? "https://plus.unsplash.com/premium_photo-1684508638760-72ad80c0055f?q=80&w=1171&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D":v,
          }, // Assuming Image is a URL or path to the image
     price: { type: Number, required: true },
     location: { type: String, },
